c55c891f077999095aa35fb1ec29fa7bd89b6ef8
11aji <jalyjaly18@gmail.com>
2023-12-08 15:14:03 +0800

    images

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -11,24 +11,28 @@ import { Head } from '@inertiajs/vue3';
     <AuthenticatedLayout>
         <div class="flex justify-center">
             <div class="p-6 dark:border-gray-700 mt-14 bg-blue-400">
+                <img src="/partas_logo.png" class="h-40 mx-auto mb-4" alt="FlowBite Logo" />
+                <h1 class="text-4xl font-bold text-center mb-8">WELCOME TO PARTAS BUS INC</h1>
+
                 <div class="grid grid-cols-2 md:grid-cols-3 gap-4">
                     <div>
-                        <img class="h-auto max-w-full rounded-lg" src="storage/images/carousel1.jpg" alt="">
+                        <img class="h-auto max-w-full rounded-lg" src="/partas-bus-1.jpg" alt="">
                     </div>
                     <div>
-                        <img class="h-auto max-w-full rounded-lg" src="storage/images/carousel2.jpg" alt="">
+                        <img class="h-auto max-w-full rounded-lg" src="/partas-bus-2.jpg" alt="">
                     </div>
                     <div>
-                        <img class="h-auto max-w-full rounded-lg" src="storage/images/carousel3.jpg" alt="">
+                        <img class="h-auto max-w-full rounded-lg" src="/partas-bus-3.jpg" alt="">
                     </div>
                     <div>
-                        <img class="h-auto max-w-full rounded-lg" src="storage/images/carousel1.png" alt="">
+                        <img class="h-auto max-w-full rounded-lg" src="/partas-bus-4.jpeg" alt="">
                     </div>
                     <div>
                         <img class="h-auto max-w-full rounded-lg" src="storage/images/carousel1.png" alt="">
                     </div>
                 </div>
             </div>
-    </div>
+        </div>
     </AuthenticatedLayout>
 </template>
+
